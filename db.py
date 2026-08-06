3644ba88d25b3bb4047db17434aa0dcf7e681ee8
Anjan kumar Mahapatro <anjanmahapatro5802@gmail.com>
2026-08-06 15:42:55 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import os
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 print("Connecting to MongoDB...")
 
@@ -8,7 +9,10 @@ MONGO_URI = os.environ.get("MONGO_URI")
 if not MONGO_URI:
     raise Exception("MONGO_URI not found in environment variables")
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    server_api=ServerApi('1')
+)
 
 db = client["evm_voting_db"]
 
@@ -17,4 +21,5 @@ admins_col = db["admins"]
 candidates_col = db["candidates"]
 votes_col = db["votes"]
 
-print("Collections:", db.list_collection_names())
+# Comment this out for now
+# print("Collections:", db.list_collection_names())
